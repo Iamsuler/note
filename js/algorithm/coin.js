7727b49ac27119d60e12dbddfc2889e18f1a7785
suler <heweifang0612@163.com>
2021-12-18 22:57:56 +0800

    调整函数条件

--- js/algorithm/coin.js
+++ js/algorithm/coin.js
@@ -2,19 +2,14 @@ var coinChange = function (coins, amount) {
   const dp = function (n) {
     if (n === 0) return 0;
     if (n < 0) return -1;
-    let res = Number.MAX_SAFE_INTEGER;
+    let res = amount + 1;
     for (let coin of coins) {
-      if (n - coin === 0) {
-        res = 1;
-        break;
-      } else if (n - coin < 0) {
-        continue;
-      }
       let sub = dp(n - coin);
+      if (sub === -1) continue;
       res = Math.min(res, 1 + sub);
     }
 
-    return res !== Number.MAX_SAFE_INTEGER ? res : -1;
+    return res !== amount + 1 ? res : -1;
   };
 
   return dp(amount);
