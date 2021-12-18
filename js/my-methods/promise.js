7620b9370051e89fcd6d747bdc5897748dcf474a
suler <heweifang0612@163.com>
2021-12-18 22:50:43 +0800

    完成自定义promise

--- js/my-methods/promise.js
+++ js/my-methods/promise.js
@@ -1,3 +1,54 @@
+// resolvePromise函数，处理自己return的promise和默认的promise2的关系
+function resolvePromise(promise, tempValue, resolve, reject) {
+  // 循环引用报错
+  if (promise === tempValue) {
+    return reject(new TypeError("Chaining cycle detected for promise"));
+  }
+
+  // 防止多次调用
+  let called = false;
+
+  // tempValue 不是null，且tempValue是对象或者函数
+  if (tempValue !== null && ["object", "function"].includes(typeof tempValue)) {
+    try {
+      // A+ 规定
+      let then = tempValue.then;
+
+      // 如果then是函数，就默认是promise
+      if (typeof then === "function") {
+        // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
+        then.call(
+          tempValue,
+          (value) => {
+            if (called) return;
+            called = true;
+
+            // resolve的结果依旧是promise 那就继续解析
+            resolvePromise(promise, value, resolve, reject);
+          },
+          (err) => {
+            if (called) return;
+            called = true;
+            reject(err);
+          }
+        );
+      } else {
+        // 直接成功即可
+        resolve(tempValue);
+      }
+    } catch (error) {
+      // 也属于失败
+      if (called) return;
+      called = true;
+
+      // 取then出错了那就不要在继续执行了
+      reject(error);
+    }
+  } else {
+    resolve(tempValue);
+  }
+}
+
 class MyPromise {
   constructor(executor) {
     // 校验executor
@@ -5,24 +56,32 @@ class MyPromise {
       throw new Error("Promise resolver is not a function");
     }
 
-    this.value = nulll;
+    this.value = null;
     this.reason = null;
     this.statu = "pending";
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
 
     const resolve = (value) => {
       if (this.statu === "pending") {
         this.statu = "fulfilled";
         this.value = value;
+        this.onFulfilledCallbacks.forEach((cb) => cb(this.value));
       }
     };
     const reject = (reason) => {
       if (this.statu === "pending") {
         this.statu = "rejected";
         this.reason = reason;
+        this.onRejectedCallbacks.forEach((cb) => cb(this.reason));
       }
     };
 
-    executor(resolve, reject);
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
   }
 
   // then 方法，里面有两个参数：onFulfilled（成功时 resolve 触发）onFulfilled（失败时 reject 触发）
@@ -41,10 +100,82 @@ class MyPromise {
       onRejected = (reason) => reason;
     }
 
-    if (this.statu === "fulfilled") {
-      onFulfilled(this.value);
-    } else if (this.statu === "rejected") {
-      onRejected(this.reason);
-    }
+    const tempPromise = new MyPromise((resolve, reject) => {
+      if (this.statu === "pending") {
+        // promise 中是异步执行时，status还未改变，先将回调函数缓存起来
+        this.onFulfilledCallbacks.push((value) => {
+          setTimeout(() => {
+            resolve(onFulfilled(value));
+          });
+        });
+        this.onRejectedCallbacks.push((reason) => {
+          setTimeout(() => {
+            // resolvePromise函数，处理自己return的promise和默认的promise2的关系
+            reject(onRejected(reason));
+          });
+        });
+      } else if (this.statu === "fulfilled") {
+        setTimeout(() => {
+          const tempValue = onFulfilled(this.value);
+          resolvePromise(tempPromise, tempValue, resolve, reject);
+        });
+      } else if (this.statu === "rejected") {
+        setTimeout(() => {
+          reject(onRejected(this.reason));
+        });
+      }
+    });
+
+    return tempPromise;
   }
 }
+
+// 测试输出顺序
+// console.log(1);
+// new MyPromise((resolve,reject)=>{
+//     console.log(2);
+//     resolve(3);
+// })
+// .then(
+//     value=>console.log(value)
+// );
+// console.log(4);
+
+// 测试报错捕获
+// new MyPromise((resolve, reject) => {
+//   throw new Error("随便抛出一个错误");
+//   resolve(3);
+// }).then(
+//   (value) => console.log(value),
+//   (reason) => console.log(reason)
+// );
+
+// 测试promise异步执行
+// new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(3);
+//   });
+// }).then((value) => console.log(value));
+
+// 测试链式调用
+// new MyPromise((resolve, reject) => {
+//   resolve(3);
+// })
+//   .then((value) => console.log(value))
+//   .then()
+//   .then((value) => console.log(value));
+
+// 测试递归调用
+// let p = new Promise((resolve) => {
+//   resolve(0);
+// }).then((data) => p);
+
+new MyPromise((resolve, reject) => {
+  resolve(3);
+})
+  .then((value) => {
+    return new MyPromise((resolve, reject) => {
+      resolve(1);
+    });
+  })
+  .then((value) => console.log("value", value));
