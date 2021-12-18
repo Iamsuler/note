e5dd250555aef12546550c7b2c7c2c6cf5b206fe
suler <heweifang0612@163.com>
2021-12-18 23:00:33 +0800

    添加备忘录

--- js/algorithm/coin.js
+++ js/algorithm/coin.js
@@ -1,15 +1,21 @@
 var coinChange = function (coins, amount) {
+  let memo = new Map();
   const dp = function (n) {
+    if (memo.has(n)) return memo.get(n);
+
+    // base case
     if (n === 0) return 0;
     if (n < 0) return -1;
     let res = amount + 1;
+
     for (let coin of coins) {
       let sub = dp(n - coin);
       if (sub === -1) continue;
       res = Math.min(res, 1 + sub);
     }
 
-    return res !== amount + 1 ? res : -1;
+    memo.set(n, res !== amount + 1 ? res : -1);
+    return memo.get(n);
   };
 
   return dp(amount);
