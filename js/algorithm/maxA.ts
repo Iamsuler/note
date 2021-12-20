9ef35358f441799d37ba9aaf57093e2d41bcb73e
suler <heweifang0612@163.com>
2021-12-20 22:54:04 +0800

    添加备忘录

--- js/algorithm/maxA.ts
+++ js/algorithm/maxA.ts
@@ -3,17 +3,25 @@
 // n = 3 => 3   n = 7 => 9
 
 function maxA(count: number): number {
+  let memo = new Map();
   function dp(n: number, num: number, copy: number): number {
+    const key = `${n}-${num}-${copy}`;
+    if (memo.has(key)) {
+      return memo.get(key);
+    }
     // base case
     if (n <= 0) {
       return num;
     }
 
-    return Math.max(
+    const res = Math.max(
       dp(n - 1, num + 1, copy), // A
       dp(n - 1, num + copy, copy), // C+V
-      dp(n - 2, num, num)
+      dp(n - 2, num, num) // C+A, C+C
     );
+
+    memo.set(key, res);
+    return memo.get(key);
   }
 
   return dp(count, 0, 0);
