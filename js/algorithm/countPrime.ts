79030135eb2fd714dffa4fddeaf6335fbfb8a4e8
suler <heweifang0612@163.com>
2021-12-23 23:45:48 +0800

    求素数

--- js/algorithm/countPrime.ts
+++ js/algorithm/countPrime.ts
@@ -8,15 +8,30 @@ function isPrime(n: number): boolean {
   return true;
 }
 
+// function countPrime(n: number): number {
+//   let count = 0;
+//   for (let i = 2; i < n; i++) {
+//     if (isPrime(i)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
 function countPrime(n: number): number {
-  let count = 0;
-  for (let i = 2; i < n; i++) {
-    if (isPrime(i)) {
-      count++;
+  const isPrime = new Array(n).fill(true);
+
+  for (let i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      // let j = i * i; j < n; j += i
+      for (let j = i * 2; j < n; j += i) {
+        isPrime[j] = false;
+      }
     }
   }
 
-  return count;
+  return isPrime.reduce((prev, next) => (next ? prev + 1 : prev), 0);
 }
 
 console.log(countPrime(10));
